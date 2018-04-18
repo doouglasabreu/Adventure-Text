d35901f3de307d0e2902e5f39bbe593310eaf15a
doouglasabreu <douglasabreu97@gmail.com>
2018-04-18 12:58:09 -0300

    terminando o motor

--- Jogo/jogo.lua
+++ Jogo/jogo.lua
@@ -13,52 +13,72 @@ local i = 1; --- I = CENA ATUAL
 		if i<0 then break end
 
 		print(cenas[i].descricao)
-		if(cenas[i].itens == null) then
-			break
-		end
 
 		comando_correto = false;
 		while not comando_correto do
+
 			local comando = io.read()
-			for k, v in pairs(cenas[i].itens) do
-
-				if v.comando_correto == comando then
-					line = comando
-					isGet = false
-					count = 0
-
-					-----------------COMANDO GET COLOCAR NO INVENTARIO---------------------
-					for token in string.gmatch(comando, "[^%s]+") do
-						if(token =="get") then
-							isGet = true
-							print(isGet)
-						end
-
-						if isGet and count > 0 then
-						table.insert(inventario,token)
-						end
-
-						count = count + 1;
-					end
-
-					if i ~= v.cena_alvo then
-						i = v.cena_alvo
-						print(i)
-						comando_correto = true
-					end
-					print(v.resultado_positivo)
-
-					break
-				end
-
-				if k == table.getn(cenas[i].itens) then
-					print("Comando incorreto")
-				end
-
+			local tokens = {}
+			for token in string.gmatch(comando, "[^%s]+") do
+				table.insert(tokens, token)
 			end
+
+			if tokens[1] == "use" then
+
+				if table.getn(tokens) == 2 then
+
+					for k, v in pairs(cenas[i].itens) do
+						if comando == v.comando_correto then
+								i = v.cena_alvo
+								comando_correto = true
+								break;
+						end
+					end
+
+				elseif table.getn(tokens) == 4 and token[3] == "with" then
+
+					for p, q in pairs(cenas[i].itens) do
+						for k, v in pairs(inventario) do
+							if q.nome == v.nome then
+								i = q.cena_alvo
+								table.remove(table, k)
+								comando_correto = true
+							end
+						end
+					end
+				end
+
+			elseif tokens[1] == "check" then
+
+				for k, v in pairs(cenas[i].itens) do
+					if tokens[2] == v.nome then
+						print(v.descricao)
+					end
+				end
+
+			elseif tokens[1] == "get" then
+
+				for k, v in pairs(cenas[i].itens) do
+					if tokens[2] == v.nome then
+						table.insert(inventario, v)
+					end
+				end
+
+			elseif comando == "inventory" then
+
+				if inventory then
+					for k, v in pairs(inventory) do
+						print(v.nome)
+					end
+				else
+					print("Inventario vazio")
+				end
+
+			else
+				print("Comando incorreto")
+			end
+
 		end
-
-
 
 	end
 
